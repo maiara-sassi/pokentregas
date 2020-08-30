d6c04e079ad26a8403f7240b3644f896fda3aecb
Maiara Sassi <maiara.sassi@gmail.com>
2020-08-30 17:31:33 -0300

    welcome init page (#1)

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import logo from 'assets/images/logo.png';
+
+
+export class Logo extends React.Component {
+    render() {
+        return (
+            <img src={logo} />
+        );
+    };
+};
